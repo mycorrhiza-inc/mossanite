04b44eaa9c7aaccdeae3e8275067cfa20a4b80c9
Nicole Venner <nvenner@protonmail.ch>
2025-02-10 10:47:33 -0700

    add stuff

--- nix/vm.nix
+++ nix/vm.nix
@@ -2,6 +2,7 @@
   imports = [
     "${inputs.nixpkgs}/nixos/modules/virtualisation/quemu-vm.nix"
   ];
+  config.virtualisation.diskImage = "iso";
 
   # Use more aggressive compression then the default.
   
